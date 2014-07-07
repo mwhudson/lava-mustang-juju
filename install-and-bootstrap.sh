e058d8ebca0a4a11093b8d03940ba66079953c5a
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-07-07 13:07:58 +1200

    try to make useful when not passed directly to lava-test-shell

--- install-and-bootstrap.sh
+++ install-and-bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
+mydir=$(dirname $(readlink -f $0))
+
 mkdir -p ~ubuntu/.ssh
-cp id_rsa* ~ubuntu/.ssh
+cp $mydir/id_rsa* ~ubuntu/.ssh
 cat ~ubuntu/.ssh/id_rsa.pub >> ~ubuntu/.ssh/authorized_keys
 cat >> ~ubuntu/.ssh/config <<EOF
 StrictHostKeyChecking no
@@ -20,38 +22,36 @@ if type -p lava-sync > /dev/null; then
     lava-network broadcast eth0
     lava-network collect eth0
 
-    if [ $(lava-group bootstrap | awk 'END { print NR }') != 1 ]; then
-        echo "There should be exactly one bootstrap node!"
-        exit 1
-    fi
-
-    export BOOTSTRAP_IP=$(lava-network query $(lava-group bootstrap) ipv4)
-    export MACHINE_IPS=
-    if [ -n "$(lava-group machine)" ]; then
-        for host in $(lava-group machine); do
-            MACHINE_IPS="$MACHINE_IPS${MACHINE_IPS:+ }$(lava-network query $host ipv4)"
-        done
-    fi
-
-    sudo -u ubuntu ssh ubuntu@$BOOTSTRAP_IP true
-    if [ -n "$MACHINE_IPS" ]; then
-       for machine_ip in $MACHINE_IPS; do
-           sudo -u ubuntu ssh ubuntu@$machine_ip true
-       done
-    fi
-    if [ "$(lava-role)" = "bootstrap" ]; then
+    if [ "$(lava-group | sort  | awk '{ print $1}' | head -n1)" = "$(lava-self)" ]; then
         is_bootstrap=yes
+
+        export BOOTSTRAP_IP=$(lava-network query $(lava-self) ipv4)
+        export MACHINE_IPS=
+        for host in $(lava-group | awk '{ print $1 }'); do
+            if [ "$host" != "$(lava-self)" ]; then
+                MACHINE_IPS="$MACHINE_IPS${MACHINE_IPS:+ }$(lava-network query $host ipv4)"
+            fi
+        done
+
+        sudo -u ubuntu ssh ubuntu@$BOOTSTRAP_IP true
+        if [ -n "$MACHINE_IPS" ]; then
+            for machine_ip in $MACHINE_IPS; do
+                sudo -u ubuntu ssh ubuntu@$machine_ip true
+            done
+        fi
     else
         is_bootstrap=no
     fi
+
 else
     export BOOTSTRAP_IP=$(ip route get 8.8.8.8 | awk 'match($0, /src ([0-9.]+)/, a)  { print a[1] }')
+    export MACHINE_IPS=
     is_bootstrap=yes
 fi
 
 if [ "$is_bootstrap" = "yes" ]; then
     apt-get install -y juju-core juju-deployer git lxc
-    ./lxc-net.sh
+    $mydir/lxc-net.sh
     sleep 10
     sudo -u ubuntu -E bash -x <<\EOF
 mkdir ~/.juju
@@ -66,14 +66,14 @@ else
     sleep 10
 fi
 EOF
+    exe=$(readlink -f $1)
+    shift
     cd ~ubuntu
-    wget -O ./juju-script $LAVA_JUJU_SCRIPT_URL
-    chmod u+x juju-script
-    chown ubuntu:ubuntu juju-script
-    sudo -u ubuntu ./juju-script
+    $exe "$@"
 fi
 
 if [ "$LAVA_SLEEP_FOR_ACCESS" = "yes" ]; then
+    echo "ssh to $(ip route get 8.8.8.8 | awk 'match($0, /src ([0-9.]+)/, a)  { print a[1] }')"
     sleep 3600
 fi
 
